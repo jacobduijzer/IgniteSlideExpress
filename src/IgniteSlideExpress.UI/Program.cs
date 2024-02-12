b8e3958667dbb2609bef33332156c448d0f5b239
Jacob Duijzer <jacob@duijzer.com>
2024-02-12 07:43:40 +0100

    Clean start layout

--- src/IgniteSlideExpress.UI/Program.cs
+++ src/IgniteSlideExpress.UI/Program.cs
@@ -1,31 +1,22 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using IgniteSlideExpress.UI.Data;
+using IgniteSlideExpress.Domain;
+using IgniteSlideExpress.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<ISessionRepository, SessionRepository>();
+builder.Services.AddSingleton(new PresentationPlayer(new IgniteSlideExpress.Infrastructure.Timer()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
